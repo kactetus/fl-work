e50585cd407fb590dd72b893e81dffe8acbcec70
Dmitry <f4ll7k@mail.ru>
2015-04-02 16:16:49 +0300

    Revert "finish test"
    This reverts commit b06b9dbc4d6362952567c22ce6d74e8f4670dc80.

--- app/language/english/admin_lang.php
+++
@@ -1,4 +0,0 @@
-<?php 
-
-/* End of file admin_lang.php */
-/* Location: .app/languages/russian/admin_lang.php */
--- app/language/english/common_lang.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-
-$lang['brand_name'] = 'fl-work';
-
-
-/* End of file common_lang.php */
-/* Location: .app/languages/english/common_lang.php */
